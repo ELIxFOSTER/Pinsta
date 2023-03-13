cf459193f7586790f1628c6d20d4e0ff5eb5cacf
Tony Vongvone <tvongvone@gmail.com>
2023-03-13 00:31:50 -0700

    post route for board

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
-from app.models import Board
+from flask import Blueprint, request, jsonify
+from app.models import Board, db
 from flask_login import current_user, login_required
+from app.forms import BoardForm
 
 
 board_routes = Blueprint("board", __name__)
@@ -19,3 +20,21 @@ def get_single(id):
 def get_boards():
     all_boards = Board.query.filter(Board.userId == current_user.id)
     return [board.to_dict(add_pins=True) for board in all_boards]
+
+@board_routes.route('/', methods=['POST'])
+@login_required
+def post_board():
+    res = request.get_json()
+
+    form = BoardForm()
+    form["csrf_token"].data = request.cookies["csrf_token"]
+
+    if form.validate_on_submit():
+        board = Board(
+            name=res['name'],
+            description=res['description'],
+            userId=current_user.id
+        )
+        db.session.add(board)
+        db.session.commit()
+        return board.to_dict()
