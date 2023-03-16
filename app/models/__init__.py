c8661c0bac7911bac885c182ff2eef8fa4a053ec
Tony Vongvone <tvongvone@gmail.com>
2023-03-16 12:55:11 -0700

    complete

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,6 +1,6 @@
 from .db import db
 from .user import User
 from .board_and_pin import Board, Pin
-from boards_pins import boards_pins
+from .boards_pins import boards_pins
 from .comment import Comment
 from .db import environment, SCHEMA
