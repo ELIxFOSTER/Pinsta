a30d8237e93d60f3ed48df823ae7f80591e71e85
Eli <elixfoster@gmail.com>
2023-03-15 19:51:43 -0700

    fixed landing

--- app/seeds/boards.py
+++ app/seeds/boards.py
@@ -20,6 +20,38 @@ def seed_boards_pins():
     p9 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/564x/5f/33/25/5f332591d9b7e77b63b35f75351bf7d2.jpg", userId=3)
     p10 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/564x/ec/14/32/ec143208b2a03a0dfe0a7332bd7add84.jpg", userId=3)
 
+    p11 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/736x/99/24/70/992470e09295d6f5478dab669064a7ce.jpg", userId=3)
+    p12 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/05/f7/09/05f7091016740e15005de37b9ab461cd.jpg", userId=3)
+
+    p13 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/40/74/c4/4074c4c5d4b806637d16fa7bc3088134.jpg", userId=3)
+    p14 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/52/ed/25/52ed25693e77086093edb89d1dc43aa3.jpg", userId=3)
+
+    p15 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/41/48/7b/41487b03f6feba4a510e1b5d48db1c1e.jpg", userId=3)
+    p16 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/c0/3b/ca/c03bca8910c45fbae65d6e74ed807847.jpg", userId=3)
+
+    p17 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/de/bb/3b/debb3b89a452eb07788b9bc0c03e9439.jpg", userId=3)
+    p18 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/26/55/92/2655929a90da10307909cc952ae9caef.jpg", userId=3)
+
+    p19 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/8e/75/33/8e7533fd23c71768e1a07478af67c038.jpg", userId=3)
+    p20 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/05/67/04/056704fb9dbb0a420cadebb02733e19c.jpg", userId=3)
+
+    p21 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/26/d3/57/26d3570a89288ebb80a563eb2e48ceec.jpg", userId=3)
+    p22 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/b2/47/50/b2475024d67d5e0b500491db61e10f59.jpg", userId=3)
+
+    p23 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/0a/83/88/0a8388086e2026665f29f8cdea67b2b2.jpg", userId=3)
+    p24 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/d6/57/9c/d6579cb2f0c9cb1ec8d162a116d4d7a9.jpg", userId=3)
+
+    p25 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/33/35/75/3335758a72f5b6622b419eba15aefab2.jpg", userId=3)
+    p26 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/9d/1e/8b/9d1e8bce9cdde4a352fada9608828dac.jpg", userId=3)
+
+    p27 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/00/9f/49/009f49e394795a4d663486aa2587178a.jpg", userId=3)
+    p28 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/00/b3/3a/00b33af9918d1c94822a834f8bca50ad.jpg", userId=3)
+
+    p29 = Pin(title="OMG", description="Song by New Jeans", imageUrl="https://i.pinimg.com/236x/f2/af/a2/f2afa20ad8d37717f1841060fc493bd1.jpg", userId=3)
+    p30 = Pin(title="Empire State Of Mind", description="Song by Jay Z and Alicia Keys", imageUrl="https://i.pinimg.com/236x/2b/fe/bb/2bfebb350c5f65affd9549febfaa363d.jpg", userId=3)
+
+
+
     b1 = Board(name="My Foods", description="My favorite foods", userId=1)
     b2 = Board(name="My Games", description="My favorite Games", userId=2)
     b3 = Board(name="My Animals", description="My favorite Animals", userId=3)
@@ -36,7 +68,7 @@ def seed_boards_pins():
     b3.board_pins.append(p6)
 
 
-    db.session.add_all([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10])
+    db.session.add_all([p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24, p25, p26, p27, p28, p29, p30])
     db.session.add_all([b1, b2, b3, b4, b5])
     db.session.commit()
 
