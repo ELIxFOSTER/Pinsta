b2a78608582c358740034e5ca4028f1dac246f26
Eli <elixfoster@gmail.com>
2023-03-17 12:15:17 -0700

    modal done

--- react-app/src/components/CreatedPinModal/index.js
+++ react-app/src/components/CreatedPinModal/index.js
@@ -3,41 +3,45 @@ import { login } from "../../store/session";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import { get_all_boards } from "../../store/board";
+import { deletePinThunk } from "../../store/pins";
+import { getCurrentUserPins } from "../../store/pins";
+import { NavLink } from "react-router-dom";
+import './CreatedPinModal.css'
 
 function CreatedPinModal({ pin }) {
   const dispatch = useDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showMenu, setShowMenu] = useState(false);
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-//     const data = await dispatch(login(email, password));
-//     if (data) {
-//       setErrors(data);
-//     } else {
-//         dispatch(get_all_boards())
-//         closeModal()
-//     }
-//   };
+  const refresh = async () => {
+    await dispatch(getCurrentUserPins());
+  };
 
-const handleClick = async(e) => {
-    e.preventDefault()
+  const handleClick = async (e) => {
+    e.preventDefault();
+    await dispatch(deletePinThunk(pin.id));
+    closeModal()
+    refresh();
+  };
 
-}
+
 
   return (
-    <div>
-        <div>Edit This Pin</div>
-        <div>
-            <img src={pin.imageUrl}></img>
-        </div>
-        <div>
-            <div>Delete</div>
-            <div>Cancel</div>
-            <div>Edit</div>
-        </div>
+    <div className='pin-modal-wrapper'>
+      <div>Edit This Pin</div>
+      <div>
+        <img src={pin.imageUrl}></img>
+      </div>
+      <div className='pin-button-container'>
+        <div className='pin-delete-button'onClick={(e) => handleClick(e)}>Delete</div>
+        <div className='pin-cancel-button' onClick={(e) => closeModal(e)}>Cancel</div>
+        <NavLink to={`/edit-pin/${pin.id}`}>
+        <div className='pin-edit-button' onClick={(e) => closeModal(e)}>Edit</div>
+        </NavLink>
+      </div>
     </div>
   );
 }
