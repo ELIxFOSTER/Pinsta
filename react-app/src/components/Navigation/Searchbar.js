908e7fdd6a8dec987c8b6034942e00f46c2d19a9
Eunice Park <eunicexpark01@gmail.com>
2023-03-19 13:10:41 -0700

    refactor

--- react-app/src/components/Navigation/Searchbar.js
+++ react-app/src/components/Navigation/Searchbar.js
@@ -1,61 +1,6 @@
-// import React, { useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { useHistory } from "react-router-dom";
+
 import { FaSearch } from "react-icons/fa";
-// import { getFilteredPins } from "../../store/pins";
 import "./Searchbar.css";
-
-// function SearchBar(props) {
-//   const [searchTerm, setSearchTerm] = useState("");
-//   const dispatch = useDispatch();
-//   const history = useHistory();
-//   const filteredPins = useSelector((state) => state.pinsReducer.FiltPins);
-
-//   function handleChange(e) {
-//     const value = e.target.value;
-//     setSearchTerm(value);
-//     dispatch(getFilteredPins({ str: value }));
-//     history.push("/filtered");
-//   }
-
-//   function handlePinClick(pinId) {
-//     history.push(`/pins/${pinId}`);
-//   }
-
-//   function handleSubmit(event) {
-//     event.preventDefault();
-//   }
-
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <div className="search-container">
-//         <FaSearch className="search-icon" />
-//         <input
-//           type="text"
-//           placeholder="Search"
-//           value={searchTerm}
-//           onChange={handleChange}
-//           className="search-input"
-//         />
-//         {filteredPins && filteredPins.length > 0 && (
-//           <ul className="search-results">
-//             {filteredPins.map((pin) => (
-//               <li
-//                 key={pin.id}
-//                 onClick={() => handlePinClick(pin.id)}
-//                 className="search-result"
-//               >
-//                 {pin.name}
-//               </li>
-//             ))}
-//           </ul>
-//         )}
-//       </div>
-//     </form>
-//   );
-// }
-
-// export default SearchBar;
 
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
@@ -64,25 +9,24 @@ import { getFilteredPins } from "../../store/pins";
 
 function SearchBar(props) {
   const [searchTerm, setSearchTerm] = useState("");
-  const dispatch = useDispatch()
-  const history = useHistory()
+  const dispatch = useDispatch();
+  const history = useHistory();
 
   function handleSubmit(e) {
     e.preventDefault();
-    // handle search here
-    dispatch(getFilteredPins({str: searchTerm}))
-    history.push('/filtered')
+    dispatch(getFilteredPins({ str: searchTerm }));
+    history.push("/filtered");
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="search-container">
-      <FaSearch className="search-icon" />
+        <FaSearch className="search-icon" />
         <input
           type="text"
           placeholder="Search"
           value={searchTerm}
-          onChange={e => setSearchTerm(e.target.value)}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className="search-input"
         ></input>
       </div>
