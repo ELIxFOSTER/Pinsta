9be68d30d9c1b532be240a0423ec9cb6192eecdb
Eunice Park <eunicexpark01@gmail.com>
2023-03-19 13:07:51 -0700

    refactor

--- react-app/src/components/Comments/index.js
+++ react-app/src/components/Comments/index.js
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
+
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 
 const Comments = () => {
   const dispatch = useDispatch();
-  // const { commentId } = useParams();
   const singleComment = useSelector((state) => state.comments.oneComment);
-  const sessionUser = useSelector((state) => state.session.user)
+  const sessionUser = useSelector((state) => state.session.user);
 
   return (
     <div className="comments-container">
       <h3> Comments </h3>
-      <span>{singleComment.comment} by {sessionUser.name}</span>
+      <span>
+        {singleComment.comment} by {sessionUser.name}
+      </span>
     </div>
   );
 };
--- react-app/src/store/comment.js
+++ react-app/src/store/comment.js
@@ -1,7 +1,8 @@
+
 const GET_COMMENTS = "comments/get_comments";
 const GET_ONE_COMMENT = "comments/get_one_comment";
-const CREATE_COMMENT = "comments/create_one"
-const DELETE_COMMENT = "comments/delet"
+const CREATE_COMMENT = "comments/create_one";
+const DELETE_COMMENT = "comments/delet";
 
 const getComments = (comment) => {
   return {
@@ -20,16 +21,16 @@ const getPcomments = (comments) => {
 const createComment = (obj) => {
   return {
     type: CREATE_COMMENT,
-    payload: obj
-  }
-}
+    payload: obj,
+  };
+};
 
-const deleteSingleComment = id => {
+const deleteSingleComment = (id) => {
   return {
     type: DELETE_COMMENT,
-    payload: id
-  }
-}
+    payload: id,
+  };
+};
 
 export const get_all_comments = () => async (dispatch) => {
   const response = await fetch("/api/comments/");
@@ -46,11 +47,11 @@ export const getPinComments = (id) => async (dispatch) => {
   if (response.ok) {
     const data = await response.json();
     dispatch(getPcomments(data));
-    return data
+    return data;
   }
 };
 
-export const createNewComment = (commentData) => async dispatch => {
+export const createNewComment = (commentData) => async (dispatch) => {
   const response = await fetch("/api/comments/new", {
     method: "POST",
     headers: {
@@ -63,25 +64,24 @@ export const createNewComment = (commentData) => async dispatch => {
     dispatch(createComment(newComment));
   } else {
     const errorData = await response.json();
-    console.log('error in this response', errorData.errors);
-    return errorData
+    console.log("error in this response", errorData.errors);
+    return errorData;
   }
 };
 
+export const deleteComment = (commentId) => async (dispatch) => {
+  const response = await fetch(`/api/comments/${commentId}`, {
+    method: "DELETE",
+  });
 
-export const deleteComment = (commentId) => async dispatch => {
-  const response =  await fetch(`/api/comments/${commentId}`, {
-    method: 'DELETE'
-  })
-
-  if(response.ok) {
-    dispatch(deleteSingleComment(commentId))
+  if (response.ok) {
+    dispatch(deleteSingleComment(commentId));
   }
-}
+};
 
 let initialState = {
   allComments: {},
-  pinComments: {}
+  pinComments: {},
 };
 
 export default function commentReducer(state = initialState, action) {
@@ -93,19 +93,19 @@ export default function commentReducer(state = initialState, action) {
       };
     }
     case GET_ONE_COMMENT: {
-      const newState = {...state, pinComments: {}}
-      action.payload.forEach(ele => newState.pinComments[ele.id] = ele)
-      return newState
+      const newState = { ...state, pinComments: {} };
+      action.payload.forEach((ele) => (newState.pinComments[ele.id] = ele));
+      return newState;
     }
     case CREATE_COMMENT: {
-      const newState = {...state, pinComments: {...state.pinComments}}
-      newState.pinComments[action.payload.id] = action.payload
-      return newState
+      const newState = { ...state, pinComments: { ...state.pinComments } };
+      newState.pinComments[action.payload.id] = action.payload;
+      return newState;
     }
     case DELETE_COMMENT: {
-      const newState = {...state, pinComments: {...state.pinComments}}
-      delete newState.pinComments[action.payload]
-      return newState
+      const newState = { ...state, pinComments: { ...state.pinComments } };
+      delete newState.pinComments[action.payload];
+      return newState;
     }
     default:
       return state;
