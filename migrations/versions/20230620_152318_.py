dcfabaaedbd90f51b625c15b769456e1d409ad41
Tony Vongvone <tvongvone@gmail.com>
2023-06-20 15:25:28 -0700

    fix deployment

--- migrations/versions/20230620_152318_.py
+++ migrations/versions/20230620_152318_.py
@@ -1,16 +1,20 @@
 """empty message
 
-Revision ID: d6ea0dafd526
-Revises: 
-Create Date: 2023-04-04 21:24:58.208469
+Revision ID: 8b5b948c6eea
+Revises:
+Create Date: 2023-06-20 15:23:18.154120
 
 """
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
-revision = 'd6ea0dafd526'
+revision = '8b5b948c6eea'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -60,6 +64,10 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+    # ### end Alembic commands ###qqqqqqqqq
     # ### end Alembic commands ###
 
 
